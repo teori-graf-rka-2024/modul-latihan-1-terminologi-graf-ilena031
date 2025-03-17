2991dc12bce3ed68105dc972517d068c0cf74629
ilena031 <5054241003@student.its.ac.id>
2025-03-17 16:07:50 +0700

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -10,7 +10,7 @@ def get_degree(G: nx.Graph, node: int) -> int:
 def dfs_traversal(G: nx.Graph, start: int) -> list[int]:
     return list(nx.dfs_preorder_nodes (G,start,depth_limit=5))
 def bfs_traversal(G: nx.Graph, start: int) -> list[int]:
-    return list(nx.bfs_layers(G,start))
+    return list(nx.bfs_tree(G, source=start).nodes())
 def find_shortest_path(G: nx.Graph, source: int, target: int) -> list[int]:
     return list(nx.shortest_path(G,source,target))
 def visualize_graph(G: nx.Graph): 
